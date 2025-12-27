42559a7868ccc01e15e24060d2df9993de0e0782
Muhammad Essam <mohamed.essam@hadafsolutions.com>
2025-12-27 19:19:03 +0200

    refactor winning conditions

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -31,16 +31,17 @@ namespace Tennis
                 return GetDrawScore();
             }
             
-            if (m_score1 >= 4 || m_score2 >= 4)
+            if (m_score1 > (int)ScoreName.Forty || m_score2 > (int)ScoreName.Forty)
             {
-                string score = "";
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                var winnerPlayer = m_score1 > m_score2 ? player1Name : player2Name;
+                var differenceInResult = m_score1 - m_score2;
+
+                if (Math.Abs(differenceInResult) > 1)
+                {
+                    return "Win for " + winnerPlayer;
+                }
              
-                return score;
+                return "Advantage " + winnerPlayer;
             }
 
             return $"{Enum.GetName(typeof(ScoreName), m_score1)}-{Enum.GetName(typeof(ScoreName), m_score2)}";
